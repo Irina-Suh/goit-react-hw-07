5e407cd1f8ca959dac3e5299aeab5d9fed2ecb18
Iryna <ikalchenko23@gmail.com>
2025-05-04 20:36:19 +0300

    fix loader

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import s from './ContactList.module.css'
 import Contact from '../Contact/Contact'
 import { useSelector } from 'react-redux'
-import { selectError, selectFilteredContacts, selectLoading } from '../../redux/contactsSlice'
+import { selectError, selectFilteredContacts } from '../../redux/contactsSlice'
 import Loader from '../Loader/Loader'
 
 const ContactList = () => {
 
   const error = useSelector(selectError);
-  const loading = useSelector(selectLoading);
+ 
 
   const filteredContacts = useSelector(selectFilteredContacts);
 
@@ -22,7 +22,7 @@ const ContactList = () => {
         </li>
       ))}
     </ul> 
-      {loading && !error && <Loader/>}
+      
 
       {error && <h2>{error}</h2>}
       </>
